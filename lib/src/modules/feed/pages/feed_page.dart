f41de810ae11d408c93e6b257bdabc5f1b5d51a9
manirahmanzadeh <72001950+manirahmanzadeh@users.noreply.github.com>
2023-08-26 15:01:08 +0330

    page structure

--- lib/src/modules/feed/pages/feed_page.dart
+++ lib/src/modules/feed/pages/feed_page.dart
@@ -1,8 +1,26 @@
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/feed_provider.dart';
 
 class FeedPage extends StatelessWidget {
-  const FeedPage({Key? key}) : super(key: key);
+  const FeedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => FeedProvider(
+        context: context,
+      ),
+      child: const _FeedPage(),
+    );
+  }
+}
+
+
+class _FeedPage extends StatelessWidget {
+  const _FeedPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
---
+++ lib/src/modules/feed/providers/feed_provider.dart
@@ -0,0 +1,10 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/src/core/safe_provider.dart';
+
+class FeedProvider extends SafeProvider {
+  final BuildContext context;
+  FeedProvider({required this.context});
+
+}
